d234c2c883a32a1171ff7fc37fc94e1adb6afc14
yudhisthereal <yudhisthereal@gmail.com>
2025-01-07 11:36:19 +0700

    all test passed

---
+++ test/settings/view/settings_page_test.dart
@@ -0,0 +1,80 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:weather_app/settings/settings.dart';
+import 'package:weather_app/weather/weather.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockWeatherCubit extends MockCubit<WeatherState>
+    implements WeatherCubit {}
+
+void main() {
+  group('SettingsPage', () {
+    late WeatherCubit weatherCubit;
+
+    setUp(() {
+      weatherCubit = MockWeatherCubit();
+    });
+
+    testWidgets('is routable', (tester) async {
+      when(() => weatherCubit.state).thenReturn(WeatherState());
+      await tester.pumpWidget(
+        BlocProvider.value(
+          value: weatherCubit,
+          child: MaterialApp(
+            home: Builder(
+              builder: (context) => Scaffold(
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.of(context).push<void>(
+                      SettingsPage.route(),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      expect(find.byType(SettingsPage), findsOneWidget);
+    });
+
+    testWidgets('calls toggleUnits when switch is changed', (tester) async {
+      whenListen(
+        weatherCubit,
+        Stream.fromIterable([
+          WeatherState(),
+          WeatherState(temperatureUnits: TemperatureUnits.fahrenheit),
+        ]),
+      );
+      when(() => weatherCubit.state).thenReturn(WeatherState());
+      await tester.pumpWidget(
+        BlocProvider.value(
+          value: weatherCubit,
+          child: MaterialApp(
+            home: Builder(
+              builder: (context) => Scaffold(
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.of(context).push<void>(
+                      SettingsPage.route(),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(Switch));
+      verify(() => weatherCubit.toggleUnits()).called(1);
+    });
+  });
+}
